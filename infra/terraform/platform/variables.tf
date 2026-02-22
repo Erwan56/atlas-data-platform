144d35e3bbe810b189eb8dc4fab9e9d759dbf291
Erwan <erwan.allain-ext@kering.com>
2026-02-22 15:08:56 +0100

    Fix Githubaction

--- infra/terraform/platform/variables.tf
+++ infra/terraform/platform/variables.tf
@@ -1,5 +1,8 @@
 variable "project_id" { type = string }
-variable "region"     { type = string default = "europe-west1" }
-variable "env"        { type = string }
+variable "region" {
+  type    = string
+  default = "europe-west1"
+}
+variable "env" { type = string }
 
 variable "terraform_admin_sa" { type = string } # tf-admin@...
